c8088590daf1c6ccb792d2cbb63533c7c9225d1b
Stiven Cardona <stiven.cardona627@gmail.com>
2024-02-08 14:30:44 -0500

    :sparkles: new Point class added

---
+++ hello-world/Point.java
@@ -0,0 +1,10 @@
+class Point {
+    private double x;
+    private double y;
+
+    public Point() {
+        this.x = 0.0;
+        this.y = 0.0;
+        System.println("Desde el constructor")
+    }
+}
