7ab46750dc01a0931b2f26e5d8c4f505924ba31b
Stiven Cardona Monsalve <stiven.cardona627@gmail.com>
2024-02-10 02:02:02 +0000

    :sparkles: new distance method added to Point class

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -1,5 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Point p1 = new Point();
+        Point p2 = new Point(4.0, 3.0);
+
+        double distance = p2.distance(p1);
+        System.out.println("Distance between p1 and p2 equal to: " + distance);
     }
 }
--- hello-world/Point.java
+++ hello-world/Point.java
@@ -1,3 +1,5 @@
+import java.lang.Math; 
+
 class Point {
     private double x;
     private double y;
@@ -5,6 +7,16 @@ class Point {
     public Point() {
         this.x = 0.0;
         this.y = 0.0;
-        System.println("Desde el constructor")
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(Point other) {
+        double dx = this.x - other.x;
+        double dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
